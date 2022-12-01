bc37556f7d532ce3c51b6a673416605aa0ba25b8
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-11-30 21:55:31 -0300

    estilizando form

--- src/components/UseForm.jsx
+++ src/components/UseForm.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const UseForm = () => {
   return (
     <div>
-        <h2>Use Form</h2>
+        <div className="form-control">
+            <label htmlFor="name">Nome:</label>
+            <input type="text" name="text" id="name" placeholder='Digite seu nome'
+            required />
+        </div>
+        <div className="form-control">
+            <label htmlFor="email">E-mail:</label>
+            <input type="email" name="email" id="email" placeholder='Digite seu Email'
+            required />
+        </div>
     </div>
   )
 }
