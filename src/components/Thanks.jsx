9bb4bb2d6e416f327d9a6b3dbaa73959395083d8
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-12-01 22:31:38 -0300

    estilizando step 3

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {
+  BsFillEmojiHeartEyesFill, 
+  BsFillEmojiSmileFill, 
+  BsFillEmojiNeutralFill, 
+  BsFillEmojiFrownFill
+} from "react-icons/bs";
+
+import "./Thanks.css"
 
 const Thanks = () => {
   return (
-    <div>
-        <h2>Thanks</h2>
+    <div className="thanks-container">
+        <h2>Falta pouco...</h2>
+        <p>A sua opnião sera importante...</p>
+        <p>Para concluir sua avaliação clique no botão enviar abaixo</p>
+        <h3>Aqui está o seu resumo:</h3>
+        <p className="review-data">
+          <span>Satisfação com o produto:</span>
+        </p>
+        <p className="review-data">
+          <span>Comentário:</span>
+        </p>
     </div>
   )
 }
